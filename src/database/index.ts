2cd9c85923a8c3c70fc658287843a2d18cd18a45
Cesar <cesarc@nicasource.com>
2023-11-02 17:31:46 -0600

    Entities in graphql service fixed

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,16 +1,19 @@
-import { MongoClient } from 'mongodb'
-import { Database } from '../lib/types'
+import { MongoClient } from "mongodb";
+import { Database, Booking, Listing, User } from "../lib/types";
 
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/proshop?retryWrites=true&w=majority`
 
-export const connectDatabase = async(): Promise<Database> => {
-    const client = await MongoClient.connect(url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
+export const connectDatabase = async (): Promise<Database> => {
+  const client = await MongoClient.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  
+  const db = client.db("main");
 
-    const db = client.db('main')
-    return {
-        listings: db.collection('test_listings')
-    }
-}
+  return {
+    bookings: db.collection<Booking>("bookings"),
+    listings: db.collection<Listing>("listings"),
+    users: db.collection<User>("users")
+  };
+};
